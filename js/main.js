467f1d2dec1059845de7475fef9d7472af314c4a
VladimirGra4evvv <gra4evvv@yandex.ru>
2021-05-28 07:26:59 +0300

    create: validate form

--- js/main.js
+++ js/main.js
@@ -53,4 +53,24 @@ $(document).ready(function(){
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+
+  //Обработка форм
+  $(".modal__form").validate({
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlenght: "Имя должно быть не короче 2 букв"
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      phone: {
+        required: "Телефон обязателен",
+      },
+    }
+  });
+
+
+
 });
